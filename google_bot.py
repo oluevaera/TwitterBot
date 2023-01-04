78caf8a0a33b1f9ceded8944c43355de2d9b6d92
Constantine <constantine.failadis@gmail.com>
2023-01-05 00:48:38 +0100

    Code refactor

---
+++ google_bot.py
@@ -0,0 +1,77 @@
+import sys
+from requests_html import HTMLSession
+import datetime
+import twitter_manager as tm
+
+
+# Get list of job cards and determine how many tweets to post.
+def get_google_job_cards():
+    session = HTMLSession()
+    url = 'https://careers.google.com/jobs/results/?location=Switzerland&q=software%20engineer&sort_by=date'
+    page = session.get(url)
+    page.html.render(sleep=1, keep_page=True, scrolldown=1)
+    
+    # Get the dates of all first page postings.
+    cards = page.html.xpath('//a[@class="gc-card"]')
+    card_dates = page.html.xpath('//div[@class="gc-card__header"]/meta[2]')
+    dates = list()
+    
+    # Posting dates are sorted by date, so:
+    # 'insert' instead of 'append' to store them from oldest[0] -> newest[19].
+    for value in range(len(card_dates)):
+        dates.insert(0, str(card_dates[value].attrs["content"]))
+
+    # Figure how many posts to create based on the last posted tweet date.
+    latest_google_tweet = tm.tweet_details_collector('Google')
+    tweet_t_pre_edit = 'T'.join(latest_google_tweet['Google'])
+    tweet_t = datetime.datetime.strptime(tweet_t_pre_edit, "%Y-%m-%dT%H:%M:%S") 
+
+    cards_length = len(cards)  
+    for count in range (cards_length):
+        web_t = datetime.datetime.strptime(dates[count][:-5], "%Y-%m-%dT%H:%M:%S")
+        if web_t > tweet_t:
+            return cards[:cards_length-count]
+
+
+# Collect the data from each Job card.
+def handle_card_data(card):
+    session = HTMLSession() 
+    url = "https://careers.google.com" + card.attrs['href']
+    page = session.get(url)
+    page.html.render(sleep=1, keep_page=True, scrolldown=1)
+    
+    # Collect the data from job posting.
+    title = card.attrs['aria-label']
+    try:
+        rendered_page_loc = page.html.xpath(
+            '//div[@class="wrapper__maincol"]//div[@itemprop="address"]/span'
+            )
+        city = rendered_page_loc[0].text
+        country = rendered_page_loc[1].text
+        #Could be used in the future. Positions total count.
+        #job_matches = page.html.xpath('//div[@data-gtm-ref="jobs-matched"]/span/*')
+    except Exception: 
+        city = "Not specified"
+        country = " -"
+
+    # Check if position is available for remote working.
+    remote_data = page.html.xpath('//li[@itemprop="jobLocationType"]')
+    if remote_data:
+        remote = remote_data[0].text
+    else:
+        remote = "Not eligible for remote working"
+    tweet = tm.tweet_text(title, url, city, country, remote, 'Google')
+    tm.create_tweet(tweet)
+
+# Control the script and exit if there are no new job postings.
+def main():
+    cards = get_google_job_cards()
+    if cards is None:
+        sys.exit()
+
+    for card in reversed(cards):
+        handle_card_data(card)
+
+
+if __name__ == "__main__":
+    main()
---
+++ twitter_manager.py
@@ -0,0 +1,71 @@
+import os
+import tweepy
+from datetime import date
+import pyshorteners
+
+auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
+auth.set_access_token(os.environ['.ACCESS_TOKEN'], os.environ['ACCESS_SECRET'])
+api = tweepy.API(auth) 
+
+def get_company_hashtag_details(company):
+    # Access the user tweets.
+    for tweet in tweepy.Cursor(
+        api.user_timeline,
+        screen_name='@GoogleJobAds',
+        tweet_mode='extended'
+        ).items():
+        
+        # Get all the hashtags per tweet
+        post_hashtags = tweet._json['entities']['hashtags']
+
+        # If there are two hashtags return date of the post and the text of the
+        # second hashtag.
+        if  len(post_hashtags) == 2:
+            if company in post_hashtags[0].values():
+                date_time = str(tweet.created_at.replace(tzinfo=None)).split(' ')
+                second_hashtag = post_hashtags[1]['text']
+                date_time.append(second_hashtag)
+                return date_time
+            
+        # Else return just the dates.
+        elif len(post_hashtags) == 1:
+            if company in post_hashtags[0].values():
+                date_time = str(tweet.created_at.replace(tzinfo=None)).split(' ')
+                return date_time
+            
+            
+# Make a dictionary containing the date and time and and #ID if it exists for
+# each company.
+def tweet_details_collector(company):
+    tweet_dates = dict()
+    date_today = date.today()
+
+    date_time = get_company_hashtag_details(company)
+    tweet_dates[company] = date_time
+    
+    return tweet_dates
+    
+
+# Function that uses all the data collected from the job postings and returns
+# the final message.
+def tweet_text(job_title, url, city, country, remote, company, hashtag=''):
+    # Shorter URL since twitter has a character limit(280).
+    type_tiny = pyshorteners.Shortener()
+    short_url = type_tiny.tinyurl.short(url)
+
+    msg = f"""
+    New position for:
+    {job_title}
+    
+    Main office: {city}, {country}
+    
+    {remote}
+    
+    Read more: {short_url}
+    #{company} {hashtag}
+    """
+    return msg
+
+    
+def create_tweet(tweet):
+    api.update_status(tweet)
