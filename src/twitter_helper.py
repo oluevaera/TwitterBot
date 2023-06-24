5abb56921c1eb695b285f87d123d8935f2efd9df
Constantine Failadis <constantine.failadis@gmail.com>
2023-06-24 02:44:49 +0200

    Update twitter_helper.py

--- src/twitter_helper.py
+++ src/twitter_helper.py
@@ -5,6 +5,7 @@ import json
 
 
 client = tweepy.Client(
+    bearer_token=os.environ['BEARER_TOKEN'],
     consumer_key=os.environ['CONSUMER_KEY'],
     consumer_secret=os.environ['CONSUMER_SECRET'],
     access_token=os.environ['ACCESS_TOKEN'],
