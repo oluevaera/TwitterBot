1e08c3b835653385767467f9304d75d23e468337
Constantine <constantine.failadis@gmail.com>
2024-01-12 19:22:46 +0100

    Transition to Selenium

--- src/google_jobs.py
+++ src/google_jobs.py
@@ -1,57 +1,72 @@
 import sys
-from requests_html import HTMLSession
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
 import twitter_helper as th
 
 
 # Get list of job cards and determine how many tweets to post.
 def get_google_job_cards():
-    session = HTMLSession()
-    url = ('https://careers.google.com/jobs/results/?location=Switzerland'
-           '&q=software%20engineer&sort_by=date'
-           )
-    page = session.get(url)
-    page.html.render(sleep=1, keep_page=True, scrolldown=1)
-    
-    # Get the dates of all first page postings.
-    cards = page.html.xpath('//li[@class="lLd3Je"]/div')
-    active_jobs_tags = list()
-    for card in cards:
-        active_jobs_tags.append(card.attrs["jsdata"].split(';')[1])
-    
-    posted_job_tags = th.read_latest_tweet_date('Google')
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()), options=chrome_options
+    )
+    url = 'https://careers.google.com/jobs/results/?location=Switzerland&q="software%20engineer"&sort_by=date'
+    driver.get(url)
+    driver.implicitly_wait(10)
 
-    to_be_posted = list()
-    for i, job in enumerate(active_jobs_tags, 1):
-        if job in posted_job_tags:
-            continue
-        
-        title = page.html.xpath(f'//li[@class="lLd3Je"][{i}]/div//h3')[0].text
-        job_link = 'https://www.google.com/about/careers/applications/jobs/results/' + job
-        job_id = job
-        try:
-            experience = page.html.xpath(f'//li[@class="lLd3Je"][{i}]//span[@class="wVSTAb"]')[0].text + ' level'
-        except IndexError:
-            experience =''
+    # Get the data of all the jobs from the careers website.
+    card_titles = driver.find_elements(By.CLASS_NAME, "QJPWVe")
+    card_url = [
+        element.get_attribute("href")
+        for element in driver.find_elements(
+            By.CSS_SELECTOR, ".WpHeLc.VfPpkd-mRLv6.VfPpkd-RLmnJb"
+        )
+    ]
+    card_ids = [card.split("results/")[1].split("-")[0] for card in card_url]
+    open_positions = list()
 
-        to_be_posted.append([title, job_link, job_id, experience])
+    # Populate a list with all the collected job data.
+    for title, id, url in zip(card_titles, card_ids, card_url):
+        job_title = title.text
+        job_link = url
+        job_id = id
+        open_positions.append([job_title, job_link, job_id])
 
-    return to_be_posted
+    driver.quit()
+    return open_positions
+
+
+# Filter the open positions based on the tag.
+def filtered_positions(open_positions, posted_positions):
+    filter_by_tag = [
+        position for position in open_positions if position[2] not in posted_positions
+    ]
+
+    return filter_by_tag
 
 
 # Control the script and exit if there are no new job postings.
 def main():
-    to_post = get_google_job_cards()
-    if to_post is None:
+    open_positions = get_google_job_cards()
+    posted_positions = th.read_latest_tweet_date("Google")
+
+    to_post = filtered_positions(open_positions, posted_positions)
+    if not to_post:
         sys.exit()
 
-    city = 'Zürich'
-    country = 'Switzerland'
+    city = "Zürich"
+    country = "Switzerland"
     for position in reversed(to_post):
-        tweet = th.tweet_text(position[0], position[1], city, country,
-                              'Google', '#' + position[2], position[3]
-                              )
+        tweet = th.tweet_text(
+            position[0], position[1], city, country, "Google", "#" + position[2]
+        )
         th.create_tweet(tweet)
-    th.write_latest_tweet_date('Google', to_post)
+    th.write_latest_tweet_date("Google", to_post)
 
 
 if __name__ == "__main__":
--- src/twitter_helper.py
+++ src/twitter_helper.py
@@ -5,59 +5,56 @@ import json
 
 
 client = tweepy.Client(
-    bearer_token=os.environ['BEARER_TOKEN'],
-    consumer_key=os.environ['CONSUMER_KEY'],
-    consumer_secret=os.environ['CONSUMER_SECRET'],
-    access_token=os.environ['ACCESS_TOKEN'],
-    access_token_secret=os.environ['ACCESS_SECRET']
+    bearer_token=os.environ["BEARER_TOKEN"],
+    consumer_key=os.environ["CONSUMER_KEY"],
+    consumer_secret=os.environ["CONSUMER_SECRET"],
+    access_token=os.environ["ACCESS_TOKEN"],
+    access_token_secret=os.environ["ACCESS_SECRET"],
 )
 
 
 def read_latest_tweet_date(company):
-    with open('src/data.json') as file:
+    with open("src/data.json") as file:
         json_data = json.load(file)
 
-    if company == 'Google':
-        google_posted_ids = [dic['id'] for dic in json_data[company]]
+    if company == "Google":
+        google_posted_ids = [dic["id"] for dic in json_data[company]]
         return google_posted_ids
-    elif company == 'Nvidia':
+    elif company == "Nvidia":
         date_today = date.today()
         date_yesterday = date_today - timedelta(days=1)
         nvidia_latest = [
-            dic['id'] for dic in json_data[company] if
-            dic['day'] == str(date_today) or dic['day'] == str(date_yesterday)
-            ]
+            dic["id"]
+            for dic in json_data[company]
+            if dic["day"] == str(date_today) or dic["day"] == str(date_yesterday)
+        ]
         return nvidia_latest
 
 
 def write_latest_tweet_date(company, data):
-    with open('src/data.json') as file:
+    with open("src/data.json") as file:
         existing_data = json.load(file)
 
     for posting in data:
-        existing_data[company].append(
-            {"day": str(date.today()), "id": posting[-2]}
-            )
+        existing_data[company].append({"day": str(date.today()), "id": posting[-2]})
     while len(existing_data[company]) > 20:
         existing_data[company].pop(0)
 
-    with open('src/data.json', 'w') as file:
+    with open("src/data.json", "w") as file:
         json.dump(existing_data, file)
 
 
 # Function that uses all the data collected from the job postings and returns
 # the final tweet text.
-def tweet_text(job_title, url, city, country, company, hashtag='', level=''):
-
+def tweet_text(job_title, url, city, country, company, hashtag=""):
     msg = (
-    f'New position for:\n'
-    f'{job_title}\n'
-    f'{level}\n'
-    f'\n'
-    f'Main office: {city}, {country}\n'
-    f'\n'
-    f'Read more: {url}\n'
-    f'#{company} {hashtag}'
+        f"New position for:\n"
+        f"{job_title}\n"
+        f"\n"
+        f"Main office: {city}, {country}\n"
+        f"\n"
+        f"Read more: {url}\n"
+        f"#{company} {hashtag}"
     )
 
     return msg
