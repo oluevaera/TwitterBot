4d5f21a981abdc0e00fc36bcae8ebc8ab8df8d47
Constantine <constantine.failadis@gmail.com>
2024-01-28 07:26:05 +0100

    Move to scraper class.

--- src/scraper.py
+++ src/scraper.py
@@ -8,51 +8,74 @@ import twitter_helper as th
 
 
 class JobScraper:
-    def __init__(self, url, title_class, id_class, date_class=None):
+    def __init__(self, url, title_css, id_css, date_css=None):
         self.url = url
-        self.title_class = title_class
-        self.id_class = id_class
-        self.date_class = date_class
+        self.title_css = title_css
+        self.id_css = id_css
+        self.date_css = date_css
         self.open_positions = []
 
-    def _get_job_details(self):
+    def _get_job_details(self, company):
         driver = WebDriver().instance
         driver.get(self.url)
         driver.implicitly_wait(10)
 
-        card_titles = driver.find_elements(By.CLASS_NAME, self.title_class)
-        card_ids = driver.find_elements(By.CLASS_NAME, self.id_class)
+        card_titles = driver.find_elements(By.CSS_SELECTOR, self.title_css)
+        if company == "Google":
+            card_urls = [
+                element.get_attribute("href")
+                for element in driver.find_elements(By.CSS_SELECTOR, self.id_css)
+            ]
+            card_ids = [card.split("results/")[1].split("-")[0] for card in card_urls]
+        elif company == "Nvidia":
+            card_urls = [element.get_attribute("href") for element in card_titles]
+            card_ids = [
+                element.text
+                for element in driver.find_elements(By.CSS_SELECTOR, self.id_css)
+            ]
 
-        if self.date_class:
-            card_dates = driver.find_elements(By.CLASS_NAME, self.date_class)
+        if self.date_css:
+            card_dates = driver.find_elements(By.CSS_SELECTOR, self.date_css)[1::2]
 
-        for title, ids in zip(card_titles, card_ids):
+        for i, (title, url, id) in enumerate(zip(card_titles, card_urls, card_ids)):
             job_title = title.text
-            job_link = title.get_attribute("href")
-            job_id = ids.text
-            if self.date_class:
-                job_date = re.sub(r"\W+", "", card_dates.text.split()[1])
-                self.open_positions.append([job_title, job_link, job_id, job_date])
+            job_url = url
+            job_id = id
+            if self.date_css:
+                job_date = re.sub(r"\W+", "", card_dates[i].text.split()[1])
+                self.open_positions.append(
+                    {
+                        "title": job_title,
+                        "url": job_url,
+                        "id": job_id,
+                        "date": job_date,
+                    }
+                )
             else:
-                self.open_positions.append([job_title, job_link, job_id])
+                self.open_positions.append(
+                    {"title": job_title, "url": job_url, "id": job_id}
+                )
 
-    def _filter_positions(self, company, posted_id):
+    def _filter_positions(self, company, posted_ids):
         # Filter by date, only applicable to Nvidia currently.
         if company == "Nvidia":
             self.open_positions = [
-                pos for pos in self.open_positions if pos[-1] in ["Today", "Yesterday"]
+                pos
+                for pos in self.open_positions
+                if pos["date"] in ["Today", "Yesterday"]
             ]
 
         # Filter by id.
         self.open_positions = [
-            pos for pos in self.open_positions if pos[2] not in posted_id
+            pos for pos in self.open_positions if pos["id"] not in posted_ids
         ]
 
-    def postitions_to_post(self, company):
-        self._get_job_details()
-        posted_positions = th.read_latest_teweet_date(company)
-        to_post = self._filter_positions(company, posted_positions)
-        return to_post
+    def positions_to_post(self, company):
+        self._get_job_details(company)
+        posted_positions = th.read_latest_tweet_date(company)
+        self._filter_positions(company, posted_positions)
+        print(self.open_positions)
+        return self.open_positions
 
 
 class WebDriver:
@@ -79,15 +102,33 @@ class WebDriver:
 def main():
     google_scraper = JobScraper(
         url='https://careers.google.com/jobs/results/?location=Switzerland&q="software%20engineer"&sort_by=date',
-        title_class="QJPWVe",
-        id_class=".WpHeLc.VfPpkd-mRLv6.VfPpkd-RLmnJb",
+        title_css="h3.QJPWVe",
+        id_css="a.WpHeLc.VfPpkd-mRLv6.VfPpkd-RLmnJb",
     )
-    google_to_post = google_scraper.postitions_to_post("Google")
 
     nvidia_scraper = JobScraper(
         url="https://nvidia.wd5.myworkdayjobs.com/NVIDIAExternalCareerSite?jobFamilyGroup=0c40f6bd1d8f10ae43ffaefd46dc7e78&locationHierarchy1=2fcb99c455831013ea52e9ef1a0032ba",
-        title_class="css-19uc56f",
-        id_class="css-h2nt8k",
-        date_class="css-129m7dg",
+        title_css="a.css-19uc56f",
+        id_css="li.css-h2nt8k",
+        date_css="dd.css-129m7dg",
     )
-    nvidia_to_post = nvidia_scraper.positions_to_post("Nvidia")
+
+    companies = {"google": google_scraper, "nvidia": nvidia_scraper}
+
+    for company in companies.keys():
+        to_post = companies[company].positions_to_post(company.capitalize())
+
+        for position in reversed(to_post):
+            tweet = th.tweet_text(
+                title=position["title"],
+                url=position["url"],
+                company=company.capitalize(),
+                hashtag="#" + position["id"],
+            )
+            th.create_tweet(tweet)
+
+        th.write_latest_tweet_date(company.capitalize(), to_post)
+
+
+if __name__ == "__main__":
+    main()
--- src/twitter_helper.py
+++ src/twitter_helper.py
@@ -3,7 +3,6 @@ import os
 from datetime import date, timedelta
 import json
 
-
 client = tweepy.Client(
     bearer_token=os.environ["BEARER_TOKEN"],
     consumer_key=os.environ["CONSUMER_KEY"],
@@ -36,7 +35,7 @@ def write_latest_tweet_date(company, data):
         existing_data = json.load(file)
 
     for posting in data:
-        existing_data[company].append({"day": str(date.today()), "id": posting[-2]})
+        existing_data[company].append({"day": str(date.today()), "id": posting["id"]})
     while len(existing_data[company]) > 20:
         existing_data[company].pop(0)
 
@@ -46,10 +45,10 @@ def write_latest_tweet_date(company, data):
 
 # Function that uses all the data collected from the job postings and returns
 # the final tweet text.
-def tweet_text(job_title, url, city, country, company, hashtag=""):
+def tweet_text(title, url, company, city="Zürich", country="Switzerland", hashtag=""):
     msg = (
         f"New position for:\n"
-        f"{job_title}\n"
+        f"{title}\n"
         f"\n"
         f"Main office: {city}, {country}\n"
         f"\n"
